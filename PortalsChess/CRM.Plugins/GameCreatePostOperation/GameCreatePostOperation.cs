62fcf4b8504e3dc5499ef32e01255ee749b09d91
Kanwalnoor Singh <kanwalnoor.s98@gmail.com>
2021-05-11 21:11:37 +1000

    Creating project; creating plugin
    todo: work on plugin to create squares on creation of new game

---
+++ PortalsChess/CRM.Plugins/GameCreatePostOperation/GameCreatePostOperation.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.ServiceModel;
+
+namespace PortalsChess
+{
+    public class GameCreatePostOperation : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+
+            // Obtain the tracing service
+            ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            // Obtain the execution context from the service provider.  
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            // The InputParameters collection contains all the data passed in the message request.  
+            if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+            {
+                // Obtain the target entity from the input parameters.  
+                Entity entity = (Entity)context.InputParameters["Target"];
+
+                // Obtain the organization service reference which you will need for  
+                // web service calls.  
+                IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+                try
+                {
+                    // Plug-in business logic goes here.
+                    Entity[] 
+                    for(int i = 0; i < 63; i++)
+                    {
+
+                    }
+                }
+
+                catch (FaultException<OrganizationServiceFault> ex)
+                {
+                    throw new InvalidPluginExecutionException("An error occurred in GameCreatePostOperation.", ex);
+                }
+
+                catch (Exception ex)
+                {
+                    tracingService.Trace("GameCreatePostOperation: {0}", ex.ToString());
+                    throw;
+                }
+            }
+        }
+    }
+}
